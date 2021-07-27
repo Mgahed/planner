075b1bc86f063c5588a8af2f8b38a2f1242d8270
Mgahed <abdelrhmanmgahed131@gmail.com>
2021-07-27 15:20:00 +0200

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,7 @@ export default {
     }
   },
   mounted() {
-    fetch('http://localhost:3000/projects')
+    fetch('https://github.com/Mgahed/planner/blob/master/data/db.json')
         .then(res => res.json())
         .then(data => this.projects = data)
         .catch(err => console.log(err.message))
