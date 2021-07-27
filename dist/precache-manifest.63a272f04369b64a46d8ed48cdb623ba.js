4791ef89d71e6e1bb9877f8055eae0efa569e5cf
Mgahed <abdelrhmanmgahed131@gmail.com>
2021-07-27 15:00:25 +0200

    adding dist subtree

---
+++ dist/precache-manifest.63a272f04369b64a46d8ed48cdb623ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146a39f47935dc2c076b",
+    "url": "/planner/css/app.3d0dce77.css"
+  },
+  {
+    "revision": "d9e9fb6d92a7524ddbb851e589b38b7a",
+    "url": "/planner/index.html"
+  },
+  {
+    "revision": "146a39f47935dc2c076b",
+    "url": "/planner/js/app.6c1560b7.js"
+  },
+  {
+    "revision": "c683803ad909ee5ccac2",
+    "url": "/planner/js/chunk-vendors.d94f735a.js"
+  },
+  {
+    "revision": "ae894ca847ad60f65abd3fd32c52c8a4",
+    "url": "/planner/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/planner/robots.txt"
+  }
+]);
---
+++ dist/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/planner/precache-manifest.63a272f04369b64a46d8ed48cdb623ba.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "project-planner"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
